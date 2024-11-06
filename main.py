01a07dc3569d1f5b0f4c672e21f412a59100a419
Lammy85 <daniela.sachs@yahoo.de>
2024-11-06 12:27:21 +0100

    dropdown menüs eingefügt

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import *
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route("/")
+db = SQLAlchemy(app)
+
+class Burger(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    broetchen = db.Column(db.String(200), nullable=True)
+    patty = db.Column(db.String(200), nullable=True)
+    salat = db.Column(db.String(200), nullable=True)
+    kaese = db.Column(db.String(200), nullable=True)
+    sauce = db.Column(db.String(200), nullable=True)
+
+@app.route('/', methods=['GET', 'POST'])
 def start_page():
+    if request.method == 'POST':
+        new_burger = Burger(
+            broetchen = request.form[''],
+            patty = request.form[''],
+            salat = request.form[''],
+            kaese = request.form[''],
+            sauce = request.form['']
+        )
+        db.session.add(new_burger)
+        db.session.commit()
+    
     return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run()
