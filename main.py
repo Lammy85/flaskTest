83978f2de247e4d02b19dd4fa67dec23fe85e76b
Lammy85 <daniela.sachs@yahoo.de>
2024-11-07 09:51:38 +0100

    speicherung von listen möglich

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import *
 
@@ -12,24 +12,35 @@ class Burger(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     broetchen = db.Column(db.String(200), nullable=True)
     patty = db.Column(db.String(200), nullable=True)
-    salat = db.Column(db.ARRAY(db.String(200)), nullable=True)
+    salat = db.Column(db.String(200), nullable=True)
     kaese = db.Column(db.String(200), nullable=True)
-    sauce = db.Column(db.ARRAY(db.String(200)), nullable=True)
+    sauce = db.Column(db.String(200), nullable=True)
 
 @app.route('/', methods=['GET', 'POST'])
 def start_page():
     if request.method == 'POST':
+
+        choice1 = request.form.getlist('salatoption')
+
+        salatchoice = ', '.join(choice1) if choice1 else 'Keine Auswahl'
+
+        choice2 = request.form.getlist('sauceoption')
+
+        saucechoice = ', '.join(choice2) if choice2 else 'Keine Auswahl'
+
         new_burger = Burger(
             broetchen = request.form['broetchen'],
             patty = request.form['patty'],
-            salat = request.form['salat'],
+            salat = salatchoice,
             kaese = request.form['kaese'],
-            sauce = request.form['sauce']
+            sauce = saucechoice
         )
         db.session.add(new_burger)
         db.session.commit()
+
+        return redirect('/')
     
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
