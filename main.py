06d7903f6dc15d2792f0208749d0f0413b61e26e
Lammy85 <daniela.sachs@yahoo.de>
2024-11-08 08:47:58 +0100

    code kommentiert

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import *
 
+#Erstellen und Konfigurieren der App
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+#Das Datenbankmodell
 
 class Burger(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,8 +20,13 @@ class Burger(db.Model):
     kaese = db.Column(db.String(200), nullable=True)
     sauce = db.Column(db.String(200), nullable=True)
 
+#Startseite
+
 @app.route('/', methods=['GET', 'POST'])
 def start_page():
+
+    #Speichern der Auswahl
+
     if request.method == 'POST':
 
         choice1 = request.form.getlist('salatoption')
@@ -42,10 +51,14 @@ def start_page():
     
     return render_template('index.html')
 
+#Seite mit Tabelle
+
 @app.route('/tabelle')
 def tabelle():
         daten = Burger.query.all()
         return render_template('burgerlist.html', daten=daten)
+
+#Löschen eines Datensatzes
 
 @app.route('/tabelle/<int:burger_id>', methods=['DELETE'])
 def delete_burger(burger_id):
